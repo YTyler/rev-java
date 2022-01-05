29582914d785506a97ae0a6dd8887caad879b632
Tyler Yates <tyates907@gmail.com>
2022-01-05 13:18:05 -0800

    add calculator example use

---
+++ src/jan5/Main.java
@@ -0,0 +1,12 @@
+package jan5;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Calculator Class Example
+        Calculator cal1 = new Calculator();
+        cal1.setNum1(2);
+        cal1.setNum2(3);
+        System.out.println("Result = " + cal1.add());
+    }
+}
