51deed3159d0b79b07b8398c12a9fc03aaad196f
Tyler Yates <tyates907@gmail.com>
2022-01-05 09:04:28 -0800

    assignment 3

---
+++ src/jan5/DisplayNumbers.java
@@ -0,0 +1,15 @@
+package jan5;
+
+import java.util.Scanner;
+
+public class DisplayNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the top of the range: ");
+        int num = scan.nextInt();
+        scan.nextLine();
+        for (int i = 0; i <= num; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
