eb8046bc0900202663323bc9600ff6e829f4c365
Tyler Yates <tyates907@gmail.com>
2022-01-05 08:59:39 -0800

    assignment 1

---
+++ src/jan5/FindLargest.java
@@ -0,0 +1,27 @@
+package jan5;
+
+import java.util.Scanner;
+
+public class FindLargest {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int first = scan.nextInt();
+        scan.nextLine();
+        System.out.print("Enter the second number: ");
+        int second = scan.nextInt();
+        scan.nextLine();
+        System.out.print("Enter the third number: ");
+        int third = scan.nextInt();
+        scan.nextLine();
+        int largest = first;
+        if (largest < second) {
+            largest = second;
+        }
+        if (largest < third) {
+            largest = third;
+        }
+        System.out.println("The largest of these numbers is " + largest);
+    }
+}
