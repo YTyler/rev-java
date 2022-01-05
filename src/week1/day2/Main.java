854054be44c45cff183f4c3eea8bfcb8da79b467
Tyler Yates <tyates907@gmail.com>
2022-01-05 13:58:37 -0800

    create rectangle example

--- src/week1/day2/Main.java
+++ src/week1/day2/Main.java
@@ -4,9 +4,22 @@ public class Main {
     public static void main(String[] args) {
 
         //Calculator Class Example
+        System.out.println("Calculator Example");
         Calculator cal1 = new Calculator();
         cal1.setNum1(2);
         cal1.setNum2(3);
         System.out.println("Result = " + cal1.add());
+        System.out.println();
+
+        //Rectangle Class Example
+        System.out.println("Rectangle Class Example");
+        Rectangle rect = new Rectangle(); //Empty Constructed Rectangle
+        rect.setDimension1(2);
+        rect.setDimension2(3);
+        Rectangle wrecked = new Rectangle(4,5); //Member Constructed Rectangle
+        System.out.println("Describe Rectangle 'rect'");
+        System.out.println(rect);
+        System.out.println("Describe Rectangle 'wrecked'");
+        System.out.println(wrecked);
     }
 }
--- src/week1/day2/Rectangle.java
+++ src/week1/day2/Rectangle.java
@@ -1,4 +1,50 @@
 package week1.day2;
 
 public class Rectangle {
+    private int dimension1;
+    private int dimension2;
+
+    public Rectangle() { //Empty Constructor
+    }
+
+    public Rectangle(int dimension1, int dimension2) { //Constructor with members
+        this.dimension1 = dimension1;
+        this.dimension2 = dimension2;
+    }
+
+    //Getters and Setters
+    public int getDimension1() {
+        return dimension1;
+    }
+
+    public void setDimension1(int dimension1) {
+        this.dimension1 = dimension1;
+    }
+
+    public int getDimension2() {
+        return dimension2;
+    }
+
+    public void setDimension2(int dimension2) {
+        this.dimension2 = dimension2;
+    }
+
+    //Functions for Area and Perimeter
+    public int area() {
+        return this.dimension1 * this.dimension2;
+    }
+
+    public int perimeter() {
+        return 2 * this.dimension1 + 2 * this.dimension2;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle {" +
+                "dimension1=" + dimension1 +
+                ", dimension2=" + dimension2 +
+                ", perimeter=" + perimeter() +
+                ", area=" + area() +
+                '}';
+    }
 }
