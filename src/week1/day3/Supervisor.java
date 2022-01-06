ab5484d73a5f03a8b74c0a1414bb347cf2ec26d6
Tyler Yates <tyates907@gmail.com>
2022-01-06 08:55:20 -0800

    finish supervisor

--- src/week1/day3/Supervisor.java
+++ src/week1/day3/Supervisor.java
@@ -1,4 +1,21 @@
 package week1.day3;
 
 public class Supervisor extends Employee {
+    private int experience;
+
+    public Supervisor() {
+    }
+
+    public Supervisor(long id, String name, int experience) {
+        super(id, name);
+        this.experience = experience;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
 }
