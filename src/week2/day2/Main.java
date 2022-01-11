ef3f9ada45262e1e099011ccd7ae25ddb2134435
Tyler Yates <tyates907@gmail.com>
2022-01-11 09:50:25 -0800

    import IOstream Student Demo

---
+++ src/week2/day2/Main.java
@@ -0,0 +1,11 @@
+package week2.day2;
+
+import week2.day2.service.ProcessStudent;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ProcessStudent processStudent = new ProcessStudent();
+        processStudent.process();
+    }
+}
