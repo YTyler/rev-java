9ce199ae9b3ce1e4db17c29eba9c291cc1634e8a
Tyler Yates <tyates907@gmail.com>
2022-01-11 16:27:27 -0800

    add exception files

---
+++ src/week2/day2/exceptions/CustomException.java
@@ -0,0 +1,18 @@
+package week2.day2.exceptions;
+
+public class CustomException extends Exception {
+    public CustomException() {
+    }
+
+    public CustomException(String message) {
+        super(message);
+    }
+
+    public CustomException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CustomException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/week2/day2/exceptions/ExceptionPractice.java
@@ -0,0 +1,7 @@
+package week2.day2.exceptions;
+
+public class ExceptionPractice {
+    public static void main(String[] args) {
+
+    }
+}
