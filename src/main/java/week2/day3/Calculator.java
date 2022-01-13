4c827f1946b45f69b40ba42e3bf311d7db324278
Tyler Yates <tyates907@gmail.com>
2022-01-12 21:31:02 -0800

    import calculator and tests

---
+++ src/main/java/week2/day3/Calculator.java
@@ -0,0 +1,14 @@
+package week2.day3;
+
+public class Calculator {
+    public int add(int num1, int num2){
+        int sum ;
+        sum = num1 + num2  ;
+        return sum;
+    }
+    public int subtract(int num1, int num2){
+        int res;
+        res = num1 - num2;
+        return res;
+    }
+}
